78d07e2e9c40ebc6d6d4994fab05f0a46adf80c2
TylerGlisson <tyler@tglisson.com>
2019-06-04 12:37:21 -0400

    creating the Chattroom class

--- scripts/chat.js
+++ scripts/chat.js
@@ -7,6 +7,18 @@ class Chatroom {
   constructor(room, username) {
     this.room = room;
     this.username = username;
-    this.chats = db.collection('chats');
+    this.chats = db.collection('chats'); 
+  }
+  async addChat(message) {
+    const now = new Date();
+    const chat = {
+      message,
+      username: this.username,
+      room: this.room,
+      created_at: firebase.firestore.Timestamp.fromDate
+    };
+    //save chat 
+    const response = await this.chats.add(chat);
+    return response;
   }
 }
