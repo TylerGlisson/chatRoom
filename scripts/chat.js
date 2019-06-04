36c70448f5697cd8df0704dc2309fff1ad19dd3d
TylerGlisson <tyler@tglisson.com>
2019-06-04 12:29:17 -0400

    framing out the chat script

---
+++ scripts/chat.js
@@ -0,0 +1,12 @@
+// adding new chats
+// setting up real-time listeners to get new chats
+// updating the username
+// updating the room
+
+class Chatroom {
+  constructor(room, username) {
+    this.room = room;
+    this.username = username;
+    this.chats = db.collection('chats');
+  }
+}
